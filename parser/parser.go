d2cb815635b234b7af5259be2e6b8b05887d8a38
tc80 <tc80@st-andrews.ac.uk>
2019-10-25 19:08:09 +0100

    need to fix this file

--- parser/parser.go
+++ parser/parser.go
@@ -1,22 +1,26 @@
-package main
+package parser
 
 import (
 	"bytes"
 	"fmt"
+	"strings"
+	"unicode"
 	"unicode/utf16"
+
+	"google.golang.org/api/docs/v1"
 )
 
-// A rune and its respective utf16 start and end indices
-type char struct {
+// Char is arune and its respective utf16 start and end indices
+type Char struct {
 	index   int64 // the utf16 inclusive start index of the rune
 	size    int64 // the size of the rune in utf16 units
 	content rune  // the rune
 }
 
-// A word as a string and its respective utf16 start and end indices
-type word struct {
+// Word is a string and its respective utf16 start and end indices
+type Word struct {
 	index   int64  // the utf16 inclusive start index of the string
-	size    int64  // the size of the word in utf16 units
+	size    int64  // the size of the Word in utf16 units
 	content string // the string
 }
 
@@ -27,7 +31,7 @@ type comment struct {
 }
 
 type parserInput interface {
-	current() *char
+	current() *Char
 	advance() parserInput
 }
 
@@ -37,15 +41,45 @@ type parserOutput struct {
 }
 
 type search struct {
-	results []*char
+	results []*Char
 	desired interface{}
 }
 
+const (
+	// sometimes cannot find begin and end
+	// need to fix
+	beginSymbol        = "~~begin~~"
+	endSymbol          = "~~end~~"
+	courierNew         = "Courier New"
+	foregroundColor    = "foregroundColor"
+	weightedFontFamily = "weightedFontFamily"
+)
+
 var (
-	comments = []comment{
+	black     = color{}
+	red       = color{1, 0, 0}
+	green     = color{0, 1, 0}
+	blue      = color{0, 0, 1}
+	white     = color{1, 1, 1}
+	slComment = comment{"//", "\n"}
+	mlComment = comment{"/*", "*/"}
+	comments  = []comment{
 		comment{"//", "\n"},
 		comment{"/*", "*/"},
 	}
+	keywords = map[string]color{
+		"public": red,
+		"static": blue,
+		"void":   green,
+	}
+	shortcuts = map[string]string{
+		"psvm":  "public static void main(String[] args) {\n\n}",
+		"if-el": "if (cond) {\n\n} else {\n\n}",
+	}
+	// comments = map[string]comment{
+	// 	slComment.startSymbol: slComment,
+	// 	mlComment.startSymbol: mlComment,
+	// }
 )
 
 // DIGIT DIGIT DOT DIGIT DIGIT DOT....
@@ -59,10 +93,10 @@ type parser func(parserInput) parserOutput
 
 type commentInput struct {
 	pos   int
-	chars []*char // to allow nil
+	chars []*Char // to allow nil
 }
 
-func (input commentInput) current() *char {
+func (input commentInput) current() *Char {
 	if input.pos >= len(input.chars) {
 		return nil
 	}
@@ -85,21 +119,6 @@ func fail(input parserInput) parserOutput {
 	return parserOutput{nil, input}
 }
 
-// Get the size of a rune in UTF-16 format
-func getUtf16RuneSize(r rune) int64 {
-	rUtf16 := utf16.Encode([]rune{r}) // convert to utf16, since indices in GDocs API are utf16
-	return int64(len(rUtf16))         // size of rune in utf16 format
-}
-
-// Get the size of a string in UTF-16 format
-func getUtf16StringSize(s string) int64 {
-	var size int64
-	for _, r := range s {
-		size += getUtf16RuneSize(r)
-	}
-	return size
-}
-
 // should i use ptr?
 // string size must be > 0
 
@@ -112,7 +131,7 @@ type isRuneFunc func(r rune) bool
 // consume until the next thing is non-null?
 func searchUntil(p parser) parser {
 	return func(input parserInput) parserOutput {
-		var results []*char
+		var results []*Char
 		output := p(input)
 		for ; output.result == nil; output = p(input) {
 			input = output.remaining
@@ -120,7 +139,7 @@ func searchUntil(p parser) parser {
 			if output.result == nil {
 				return success(search{results, nil}, input)
 			}
-			results = append(results, output.result.(*char))
+			results = append(results, output.result.(*Char))
 			input = output.remaining
 		}
 		input = output.remaining
@@ -140,18 +159,25 @@ func selectAny(parsers []parser) parser {
 	}
 }
 
-func separateComments(chars []*char) ([]*char, []*word) {
+func getFillerChar(index int64) *Char {
+	return &Char{index, 1, ' '}
+}
+
+// SeparateComments does...
+func SeparateComments(chars []*Char) ([]*Char, []*Word) {
 	var commentParsers []parser
 	for _, c := range comments {
 		commentParsers = append(commentParsers, expectComment(c.startSymbol, c.endSymbol))
 	}
-	var cs []*char
-	var ws []*word
+	var cs []*Char
+	var ws []*Word
 	var input parserInput = commentInput{0, chars}
 	for input != nil && input.current() != nil {
 		output := selectAny(commentParsers)(input)
 		if output.result != nil {
-			ws = append(ws, output.result.(*word)) // got a comment
+			w := output.result.(*Word)
+			ws = append(ws, w)                      // got a comment
+			cs = append(cs, getFillerChar(w.index)) // put filler in for something like hello/**/world so it is hello world instead of helloworld
 			input = output.remaining
 			continue
 		}
@@ -168,7 +194,7 @@ func expectComment(start string, end string) parser {
 			return fail(input)
 		}
 		input = output.remaining
-		w := output.result.(*word)
+		w := output.result.(*Word)
 		var b bytes.Buffer
 		b.WriteString(w.content)
 		output = searchUntil(expectWord(end))(input)
@@ -178,7 +204,7 @@ func expectComment(start string, end string) parser {
 			b.WriteRune(r.content)
 		}
 		if s.desired != nil {
-			desired := s.desired.(*word)
+			desired := s.desired.(*Word)
 			w.index += desired.size
 			b.WriteString(desired.content)
 		}
@@ -190,15 +216,15 @@ func expectComment(start string, end string) parser {
 
 func expectWord(s string) parser {
 	return func(input parserInput) parserOutput {
-		var w *word = nil
+		var w *Word = nil
 		for _, r := range s {
 			output := expectChar(isRune(r))(input)
 			if output.result == nil {
 				return fail(input)
 			}
-			c := output.result.(*char)
+			c := output.result.(*Char)
 			if w == nil {
-				w = &word{c.index, 0, s}
+				w = &Word{c.index, 0, s}
 			}
 			w.size += c.size
 			input = output.remaining
@@ -232,30 +258,227 @@ func expectChar(desired isRuneFunc) parser {
 	}
 }
 
-func getSlice(s string) []*char {
-	var cs []*char
+func getSlice(s string) []*Char {
+	var cs []*Char
 	for _, r := range s {
-		cs = append(cs, &char{0, 0, r})
+		cs = append(cs, &Char{0, 0, r})
 	}
 
 	return cs
 }
 
-// replace comment with space
-func main() {
-	cs := getSlice("te// /*\nstinhig blah blah /* hello there */ what up")
-	czz, wzz := separateComments(cs)
-	for _, c := range czz {
-		fmt.Printf("%c\n", c.content)
-	}
-	_ = czz
-	_ = wzz
-	// for _, w := range wzz {
-	// 	fmt.Println(w)
-	// }
-	// w := output.result.(*word)
-	// fmt.Printf("\n%v", w.content)
-	// commInput = output.remaining.(commentInput)
-	// fmt.Printf("\n\n%c\n", commInput.current().content)
-	//fmt.Printf("\noutput: %s, remain: %v", output.result, output.remaining.(commentInput).chars[1:])
+// An RGB color
+type color struct {
+	red   float64 // the red value from 0.0 to 1.0
+	green float64 // the green value from 0.0 to 1.0
+	blue  float64 // the blue value from 0.0 to 1.0
 }
+
+// Comment
+type comment struct {
+	startSymbol string
+	endSymbol   string
+}
+
+func getWords(chars []*Char) []*Word {
+	var words []*Word
+	var b bytes.Buffer
+	var index int64
+	start := true
+	for _, Char := range chars {
+		if unicode.IsSpace(Char.content) {
+			str := b.String()
+			if len(str) > 0 {
+				size := getUtf16StringSize(str)
+				words = append(words, &Word{index, size, str})
+				start = true
+				b.Reset()
+			}
+			continue
+		}
+		if start {
+			index = Char.index
+			start = false
+		}
+		b.WriteRune(Char.content)
+	}
+	str := b.String()
+	if len(str) > 0 {
+		size := getUtf16StringSize(str)
+		words = append(words, &Word{index, size, str})
+	}
+	return words
+}
+
+// Get the size of a rune in UTF-16 format
+func getUtf16RuneSize(r rune) int64 {
+	rUtf16 := utf16.Encode([]rune{r}) // convert to utf16, since indices in GDocs API are utf16
+	return int64(len(rUtf16))         // size of rune in utf16 format
+}
+
+// Get the size of a string in UTF-16 format
+func getUtf16StringSize(s string) int64 {
+	var size int64
+	for _, r := range s {
+		size += getUtf16RuneSize(r)
+	}
+	return size
+}
+
+// Get the slice of chars, where each Char holds a rune and its respective utf16 range
+func getChars(doc *docs.Document) []*Char {
+	var chars []*Char
+	begin := false
+	for _, elem := range doc.Body.Content {
+		if elem.Paragraph != nil {
+			for _, par := range elem.Paragraph.Elements {
+				if par.TextRun != nil {
+					content := strings.TrimSpace(par.TextRun.Content)
+					fmt.Println(content)
+					if strings.EqualFold(content, endSymbol) {
+						return chars
+					}
+					if !begin {
+						if strings.EqualFold(content, beginSymbol) {
+							begin = true
+						}
+						continue
+					}
+					index := par.StartIndex
+					// iterate over runes
+					for _, r := range par.TextRun.Content {
+						size := getUtf16RuneSize(r)                  // size of run in utf16 units
+						chars = append(chars, &Char{index, size, r}) // associate runes with ranges
+						index += size
+					}
+				}
+			}
+		}
+	}
+	return chars
+}
+
+func getDocumentRequest() *docs.Request {
+	return &docs.Request{
+		UpdateDocumentStyle: &docs.UpdateDocumentStyleRequest{
+			Fields: "background",
+			DocumentStyle: &docs.DocumentStyle{
+				Background: &docs.Background{
+					Color: &docs.OptionalColor{
+						Color: &docs.Color{
+							RgbColor: &docs.RgbColor{
+								Blue:  black.blue,
+								Red:   black.red,
+								Green: black.green,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+// Gets a request to change the color of a range.
+func getColorRequest(c color, startIndex, endIndex int64) *docs.Request {
+	return &docs.Request{
+		UpdateTextStyle: &docs.UpdateTextStyleRequest{
+			Fields: foregroundColor,
+			Range: &docs.Range{
+				StartIndex: startIndex,
+				EndIndex:   endIndex,
+			},
+			TextStyle: &docs.TextStyle{
+				ForegroundColor: &docs.OptionalColor{
+					Color: &docs.Color{
+						RgbColor: &docs.RgbColor{
+							Red:   c.red,
+							Blue:  c.blue,
+							Green: c.green,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+// Gets the requests to delete a Word and insert a new one in its place.
+func getReplaceRequest(Word *Word, wordsAfter []*Word, replace string) []*docs.Request {
+	// delete Word
+	delete := &docs.Request{
+		DeleteContentRange: &docs.DeleteContentRangeRequest{
+			Range: &docs.Range{
+				StartIndex: Word.index,
+				EndIndex:   Word.index + Word.size,
+			},
+		},
+	}
+	// insert replacement at deleted Word's location
+	insert := &docs.Request{
+		InsertText: &docs.InsertTextRequest{
+			Text: replace,
+			Location: &docs.Location{
+				Index: Word.index,
+			},
+		},
+	}
+	requests := []*docs.Request{delete, insert}
+	newSize := getUtf16StringSize(replace)
+	diff := newSize - Word.size
+	Word.size = newSize
+	// update ranges for words that follow this Word
+	for _, w := range wordsAfter {
+		w.index += diff
+	}
+	return requests
+}
+
+func getFontRequest(startIndex, endIndex int64) *docs.Request {
+	return &docs.Request{
+		UpdateTextStyle: &docs.UpdateTextStyleRequest{
+			Fields: weightedFontFamily,
+			Range: &docs.Range{
+				StartIndex: startIndex,
+				EndIndex:   endIndex,
+			},
+			TextStyle: &docs.TextStyle{
+				WeightedFontFamily: &docs.WeightedFontFamily{
+					FontFamily: courierNew,
+				},
+			},
+		},
+	}
+}
+
+func getBatchUpdate(requests []*docs.Request) *docs.BatchUpdateDocumentRequest {
+	return &docs.BatchUpdateDocumentRequest{
+		Requests: requests,
+	}
+}
+
+func getRange(chars []*Char) (int64, int64) {
+	startIndex := chars[0].index
+	lastChar := chars[len(chars)-1]
+	endIndex := lastChar.index + lastChar.size
+	return startIndex, endIndex
+}
+
+// // replace comment with space
+// func main() {
+// 	cs := getSlice("testing/* hello there */what up")
+// 	czz, wzz := SeparateComments(cs)
+// 	for _, c := range czz {
+// 		fmt.Printf("%c\n", c.content)
+// 	}
+// 	_ = czz
+// 	_ = wzz
+// 	// for _, w := range wzz {
+// 	// 	fmt.Println(w)
+// 	// }
+// 	// w := output.result.(*Word)
+// 	// fmt.Printf("\n%v", w.content)
+// 	// commInput = output.remaining.(commentInput)
+// 	// fmt.Printf("\n\n%c\n", commInput.current().content)
+// 	//fmt.Printf("\noutput: %s, remain: %v", output.result, output.remaining.(commentInput).chars[1:])
+// }
