42a1a2c2bab9d1772362d11dda5dc675c202954a
tc80 <tylercaslin47@gmail.com>
2020-09-25 01:04:30 +0100

    add shortcut file

---
+++ style/shortcut.go
@@ -0,0 +1,11 @@
+package style
+
+const (
+	// DefaultShortcutSetting is whether shortcuts are enabled by
+	// default.
+	DefaultShortcutSetting = false
+
+	// ShortcutsDirective is an optional directive to specify if shortcuts are enabled.
+	// By default, shortcuts are disabled.
+	ShortcutsDirective = "#shortcuts"
+)
